dc200acf0b7049e1037b74370a8d883fb7bce15b
mohamadjalali <mohamadjalali324@gmail.com>
2023-04-12 16:27:07 +0330

    Leave a comment to clarify how the top_one algorithm works.

--- algorithms/top_one.py
+++ algorithms/top_one.py
@@ -1,3 +1,19 @@
+"""
+This algorithm receives an array and returns most_frequent_value
+Also, sometimes it is possible to have multiple 'most_frequent_value's,
+so this function returns a list. This result can be used to find a 
+representative value in an array.
+This algorithm gets an array, makes a dictionary of it,
+finds the most frequent count, and makes the result list.
+For example: top([1, 1, 2, 2, 3, 4], 2) will return ([1, 2], 2)
+In other words the output of a tuple contains the value or values in
+the array and the highest count of that value.
+
+(TL:DR) Get mathematical Mode
+Complexity: O(n)
+"""
+
+
 def top(arr):
     values = {}
     result = []
@@ -22,7 +38,10 @@ def top(arr):
     return result, f_val
     
 
-a = [1, 2, 2, 3, 3, 5, 6, 3, 7, 7, 2, 9]
 
 if __name__ == '__main__':
+    a = [1, 2, 2, 3, 3, 5, 6, 3, 7, 7, 2, 9]
+    b = [1, 1, 2, 2, 3, 4]
     print(top(a))
+    print(top(b))
+
