58c9fa8d8ae1c3326aeb301fa2266fe774752773
mohamadjalali <mohamadjalali324@gmail.com>
2023-04-21 01:03:25 +0330

    Revert "Modify rotate algorithm....."
    This reverts commit 4db763fbec42e532dc15d47ad75c156c44027425.
       ....

--- algorithms/rotate.py
+++ algorithms/rotate.py
@@ -1,14 +1,9 @@
 
-
-def rotate(s, k):
+def rotate(s, r):
     double_s = s + s
 
-    if k <= len(s):
-        return double_s[k:k+len(s)]
-    else:
-        return double_s[k-len(s):k]
+    if r <= len(s):
+        return double_s[r:len(s)]
 
 
-if __name__ == '__main__':
-    print(rotate("Hello", 2))
-    print(rotate("World", 3))
+print(rotate("HassanHassan", 2))
