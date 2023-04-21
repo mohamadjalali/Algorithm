aa7e34ff4cc1fec1bb1ef3b26ae14fc376d65f52
mohamadjalali <mohamadjalali324@gmail.com>
2023-04-21 23:46:49 +0330

    Adding 'insert' and 'set_value' functions in the linked_list module.

--- algorithms/linked_list.py
+++ algorithms/linked_list.py
@@ -11,6 +11,7 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
+    
     def print_list(self):
         temp = self.head
         while temp is not None:
@@ -18,6 +19,7 @@ class LinkedList:
             temp = temp.next
         print("None")
 
+    
     def append(self, value):
         new_node = Node(value)
         if self.head is None:
@@ -42,6 +44,7 @@ class LinkedList:
         self.length += 1
         return True
 
+    
     def pop_first(self):
         if self.length == 0:
             return None
@@ -53,14 +56,38 @@ class LinkedList:
             self.tail = None
         return temp
     
+    
     def get(self, index):
         if index < 0 or index >= self.length:
             return None
         temp = self.head
         for _ in range(index):
             temp = temp.next
-        return temp.value
+        #return temp.value
+        return temp
 
+    
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    
+    def insert(self, index, value):
+        
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        return new_node.value
 
 
 if __name__ == "__main__":
@@ -68,6 +95,7 @@ if __name__ == "__main__":
     my_linked_list.append(8)
     my_linked_list.append(9)
     my_linked_list.prepend(1)
+    my_linked_list.set_value(3, 10)
+    my_linked_list.insert(2, 7)
     my_linked_list.print_list()
-    print(my_linked_list.get(2))
 
