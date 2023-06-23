5bf4a8871a4b1e8b94cd0bc85cdfcaf24091abb6
mohamadjalali <mohamadjalali324@gmail.com>
2023-06-23 23:32:29 +0330

    Changing code with inline comments in quick_sorted.py

--- algorithms/quick_sorted.py
+++ algorithms/quick_sorted.py
@@ -21,6 +21,31 @@ It's worth noting that the implementation assumes that the pivot function is alr
 """
 
 
+def swap(my_list, index1, index2):
+    # a list and two indexes are received as parameters, and then the two indexes in the list are moved together.
+    my_list[index1], my_list[index2] = my_list[index2], my_list[index1]
+
+
+def pivot(my_list, pivot_index, end_index):
+    # Initialize the swap index to the pivot index
+    swap_index = pivot_index
+    
+    # Iterate over the list from the pivot index + 1 to the end index
+    for i in range(pivot_index+1, end_index+1):
+        # If the current element is less than the pivot element
+        if my_list[i] < my_list[pivot_index]:
+            # Increment the swap index
+            swap_index += 1
+            # swap the current element with the element at the swap index
+            swap(my_list, swap_index, i)
+    
+    # swap the pivot element with the element at hte swap index
+    swap(my_list, pivot_index, swap_index)
+    # return the index of the pivot element after swapping
+    return swap_index
+
+
+
 def quick_sort_helper(my_list, left, right):
     # check if there is more than one element in the sublist to be sorted
     if left < right:
@@ -37,36 +62,16 @@ def quick_sort_helper(my_list, left, right):
     return my_list
 
 
-# *** OUTPUT ***
-def swap(my_list, index1, index2):
-    temp = my_list[index1]
-    my_list[index1] = my_list[index2]
-    my_list[index2] = temp
-
-
-def pivot(my_list, pivot_index, end_index):
-    swap_index = pivot_index
-
-    for i in range(pivot_index+1, end_index+1):
-        if my_list[i] < my_list[pivot_index]:
-            swap_index += 1
-            swap(my_list, swap_index, i)
-    swap(my_list, pivot_index, swap_index)
-    return swap_index
-
 
 def quick_sort(my_list):
-    quick_sort_helper(my_list, 0, len(my_list)-1)
-
+    return quick_sort_helper(my_list, 0, len(my_list)-1)
 
 
 if __name__ == "__main__":
 
     my_list = [4,6,1,7,3,2,5]
 
-    quick_sort(my_list)
-
-    print(my_list)
+    print(quick_sort(my_list))
 
 
     """
